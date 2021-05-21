38cfab3b18f960fd97fd52b5aac473fa02569aad
perepadin <rabota_1810>
2021-05-21 14:19:08 +0300

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int ticketPrice = 14_555;
-        int bonusMiles = service.calculateBonus(ticketPrice);
-
-        System.out.println("Цена билета:");
-        System.out.println(ticketPrice);
-
+        int bonusMiles = service.calculateBonus(20100);
         System.out.println("Всего начислено бонусных миль:");
         System.out.println(bonusMiles);
     }
